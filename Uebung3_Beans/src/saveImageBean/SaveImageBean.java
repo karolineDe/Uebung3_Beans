797ed0b051d7cb512913b353ecf4dd8646929659
Karoline <Karoline@KaroLappi>
2015-12-06 23:59:03 +0100

    fixed save

--- Uebung3_Beans/src/saveImageBean/SaveImageBean.java
+++ Uebung3_Beans/src/saveImageBean/SaveImageBean.java
@@ -2,116 +2,79 @@ package saveImageBean;
 
 import java.awt.image.BufferedImage;
 import java.beans.PropertyChangeEvent;
-import java.beans.PropertyChangeListener;
-import java.beans.PropertyChangeSupport;
 import java.beans.PropertyVetoException;
 import java.io.File;
 import java.io.IOException;
-import java.io.Serializable;
-import java.io.StreamCorruptedException;
 
 import javax.imageio.ImageIO;
-import javax.media.jai.PlanarImage;
 
 import interfaces.ImageListener;
+import util.FilePathVetoable;
 import util.ImageEvent;
 import util.ImageEventHandler;
+import util.TargetDescriptor;
 
-public class SaveImageBean extends ImageEventHandler implements ImageListener, PropertyChangeListener, Serializable {
+public class SaveImageBean extends ImageEventHandler implements ImageListener{
 
-	private String _savePath = "";
-	private PlanarImage _buffImage;
-	private PropertyChangeSupport _pcs;
+	@TargetDescriptor
+    private String _savePath = "out.png";
+	
+	private ImageEvent _imageEvent;
+	private String _imagePath = " ";
+	private String _fileEnding = "png";
 
-	public SaveImageBean(String path) {
-		_savePath = path;
-		_pcs = new PropertyChangeSupport(this);
-		_pcs.addPropertyChangeListener(this);
+	public SaveImageBean(){
+		super();
 	}
-
-	public SaveImageBean(String path, PlanarImage image) {
-		_savePath = path;
-		_buffImage = image;
-		_pcs = new PropertyChangeSupport(this);
-		_pcs.addPropertyChangeListener(this);
-	}
-
-	public SaveImageBean(PlanarImage image) {
-		_buffImage = image;
-		_pcs = new PropertyChangeSupport(this);
-		_pcs.addPropertyChangeListener(this);
-	}
-
-	public SaveImageBean() {
-		_savePath = "rsrc/image";
-		_pcs = new PropertyChangeSupport(this);
-		_pcs.addPropertyChangeListener(this);
-	}
+	
 
 	public String getSavePath() {
-
 		return _savePath;
 	}
 
-	public void setSavePath(String path) {
+	public void setSavePath(String path) throws PropertyVetoException{
 		String oldSavePath = _savePath;
-		String newSavePath = path;
+		 fireVetoableChange(this, _imagePath, oldSavePath, path);
 
-		_savePath = path;
-
-		_pcs.firePropertyChange("savePath", oldSavePath, newSavePath);
-	}
-
-	public PlanarImage getImage() {
-		return _buffImage;
-	}
-
-	public void setImage(PlanarImage image) {
-		_buffImage = image;
+		 _savePath = path;
+        firePropertyChange(this, _imagePath, oldSavePath, path);
 	}
 
 	@Override
-	public void onImage(ImageEvent e) {
+    @TargetDescriptor
+    public void onImage(ImageEvent event) {
+		_imageEvent = event;
 
-		try {
-			_buffImage = e.getImage();
-			File outputFile = new File(_savePath + ".jpg");
-			ImageIO.write(_buffImage, "jpg", outputFile);
+        if (event != null) {
+            try {
+                BufferedImage image = event.getImage().getAsBufferedImage();
+                ImageIO.write(image, _fileEnding, new File(_savePath));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-		} catch (StreamCorruptedException exception) {
-			exception.printStackTrace();
-		} catch (IOException exception) {
-			exception.printStackTrace();
-		}
-	}
+    @Override
+    protected void reload() {
+        onImage(_imageEvent);
+    }
+	
+    @Override
+    public void vetoableChange(PropertyChangeEvent evt) throws PropertyVetoException {
+        String propertyName = evt.getPropertyName();
 
-	@Override
-	public void propertyChange(PropertyChangeEvent ev) {
+        if (propertyName != null) {
 
-		String newPath = ev.getNewValue().toString();
-		_savePath = newPath;
-		File outputFile = new File(_savePath + ".jpg");
+            switch (propertyName) {
 
-		if (_buffImage != null) {
-			try {
-				ImageIO.write(_buffImage, "jpg", outputFile);
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-		}
+                case "png": {
+                    FilePathVetoable.validate(evt);
+                    break;
+                }
+            }
+        }
+    }
 
-	}
-
-	@Override
-	public void vetoableChange(PropertyChangeEvent evt) throws PropertyVetoException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	protected void reload() {
-		// TODO Auto-generated method stub
-		
-	}
+	
 }
